bc1cf8fc6ce86d963769319992b91a8f16ec7ddd
Khalid Bin Waheed <168022594+Khalidbinwaheed@users.noreply.github.com>
2025-04-27 23:50:45 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+# IMPORTANT: Add this file to your .gitignore!
+DB_CONFIG = {
+    'host': 'localhost',      # Or your MySQL server IP/hostname
+    'user': 'your_db_user',   # Replace with your MySQL username
+    'password': 'your_db_password', # Replace with your MySQL password
+    'database': 'hotelmanagment' # The database name you created
+}
